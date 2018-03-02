390b1211a3b2995de0b5b9c26f53ecb0b562a211
ndukler <ned36@cornell.edu>
2018-03-02 11:17:44 -0500

    Can now include backround states

--- R/callsToBed.R
+++ R/callsToBed.R
@@ -1,7 +1,7 @@
 ## A method to convert viterbi or marginal paths to a 0-based [,) formated bed file containing state and species labels
 
 #' @export
-callsToBed <- function(hmm,marginal=NULL,viterbi=NULL,marginalThresh=0.5){
+callsToBed <- function(hmm,marginal=NULL,viterbi=NULL,marginalThresh=0.5,include.background=FALSE){
     if(is.null(marginal) & is.null(viterbi)){
         stop("Either marginal or viterbi path must be passed to function.")
     }
@@ -17,18 +17,25 @@ callsToBed <- function(hmm,marginal=NULL,viterbi=NULL,marginalThresh=0.5){
     ## Get chromosomal loci of all bins that are not the full background state
     loci=list()
     for(i in 1:length(states)){
-        end=cumsum(states[[i]]$lengths)[which(states[[i]]$values>1)] ## 0-based end bin indicies [,)
-        start=end-states[[i]]$lengths[which(states[[i]]$values>1)]
+        if(include.background){
+            incl=rep(TRUE,length(states[[i]]$values))
+        } else {
+            incl=(states[[i]]$values>1)
+        }
+        end=cumsum(states[[i]]$lengths)[incl] ## 0-based end bin indicies [,)
+        start=end-states[[i]]$lengths[incl]
         if(length(start>0)){
             loci[[as.character(i)]]=data.table::data.table(chrom=hmm$emission$invariants$bed[i,]$chrom,
                     start=hmm$emission$invariants$bed[i,]$start + hmm$emission$invariants$binSize * start,
                     end=hmm$emission$invariants$bed[i,]$start + end*hmm$emission$invariants$binSize,
-                    state=states[[i]]$values[states[[i]]$values>1])            
+                    state=states[[i]]$values[incl])            
         }
     }
     loci=data.table::rbindlist(loci)
     ## Add species that have active elements in each state
     loci[,species:=paste0(hmm$emission$invariants$tree$tip.label[as.logical(hmm$emission$invariants$treeStates[state[1],])],collapse=";"),by="state"]
+    ## Use placeholder string for state 1
+    loci[state==1,species:="-"]
     loci=loci[order(chrom,start,end)]
     setcolorder(loci, c("chrom", "start", "end","species","state"))
     return(loci)
