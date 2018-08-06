e9f6f6f129120e536194dc80ddba33fdaad4b5d5
ndukler <ned36@cornell.edu>
2018-08-06 15:02:29 -0400

    Fix fitting and improve consistency

--- R/fitPhyloHMM.R
+++ R/fitPhyloHMM.R
@@ -2,6 +2,11 @@ rhoI_Ineq <- function(x,hmmObj,scale=1){
     if (length(x) != sum(!hmmObj$transition$fixed) + sum(!hmmObj$emission$fixed)) {
         stop("Length of x does not match the number of expected parameters")
     }
+    ## Save parameter values
+    eparms=hmmObj$emission$params
+    tparms=hmmObj$transition$params
+    
+    ## Overwrite object parameters with new parameter values
     hmmObj$emission$params[!hmmObj$emission$fixed] = x[1:sum(!hmmObj$emission$fixed)]
     hmmObj$transition$params[!hmmObj$transition$fixed] = x[1:sum(!hmmObj$transition$fixed) + 
                                 sum(!hmmObj$emission$fixed)]
@@ -13,8 +18,16 @@ rhoI_Ineq <- function(x,hmmObj,scale=1){
         tips = tt$tips, logBaseFreq = lbf))
     rho.inact = 1 - ((1 - hmmObj$transition$params[hmmObj$transition$getParamIndicies("autocor.active")]) * 
         ((1 - exp(p.so))/exp(p.so)))
-    return(rho.inact)
-   
+    ## Write constraint status out to log file
+    fp=file.path(hmmObj$logDir,"parameterUpdateTimer.txt")
+    write("---Evaluating Constraint status---",file=fp,append=TRUE)
+    write(paste("Transition parameter vector", paste(hmmObj$transition$params,collapse=",")),file=fp,append=TRUE)
+    write(paste0("rho.inact=", rho.inact),file=fp,append=TRUE)
+    ## Restore original parameter values
+    hmmObj$emission$params=eparms
+    hmmObj$transition$params=tparms
+    ## return rho.inact
+    return(rho.inact) 
 }
 
 #' @export
@@ -32,8 +45,8 @@ fitPhyloHMM <- function(hmm,nthreads=1){
         final.params=Rsolnp::solnp(fun=updateAllParams, pars=c(hmm$emission$params[!hmm$emission$fixed],hmm$transition$params[!hmm$transition$fixed]),
             LB = c(hmm$emission$lowerBound[!hmm$emission$fixed],hmm$transition$lowerBound[!hmm$transition$fixed]),
             UB = c(hmm$emission$upperBound[!hmm$emission$fixed],hmm$transition$upperBound[!hmm$transition$fixed]),
-            ineqfun=rhoI_Ineq, ineqLB=10^-2,ineqUB=1,
-            hmmObj=hmm,scale=-1/nsites,control=list(delta=10^-6,tol=10^-6,rho=5))
+            ineqfun=rhoI_Ineq, ineqLB=0.05,ineqUB=1,
+            hmmObj=hmm,scale=-1/nsites,control=list(delta=10^-6,tol=10^-7,rho=5))
         hmm$convergence=final.params$convergence
         write(paste0(paste0(rep("-",30),collapse=""),"END",paste0(rep("-",30),collapse="")),stdout())
         sink()
