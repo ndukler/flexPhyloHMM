169f1d6f04872c680d10786916961c7ae843281b
ndukler <ned36@cornell.edu>
2018-08-06 15:04:20 -0400

    Adding dynamic jitter term to deal with precision induced stability issue in P(s_C)

--- src/absorbingStateLogProb.cpp
+++ src/absorbingStateLogProb.cpp
@@ -7,7 +7,9 @@ using namespace Rcpp;
 NumericVector absorbingStateLogProb(NumericMatrix& conditionalEmisLogProb, NumericVector& dataLogProb, NumericVector& enumLogProb, double absorbLogProb ){
   NumericVector asLogProb(conditionalEmisLogProb.nrow());
   for(int i=0; i<conditionalEmisLogProb.nrow();i++){
-    asLogProb(i)=logMinusExp(dataLogProb(i),conditionalEmisLogProb(i,_)+enumLogProb)-absorbLogProb;
+    double jitter = logSumExp(dataLogProb(i),dataLogProb(i)-29.0);
+    // double jitter = dataLogProb(i);
+    asLogProb(i)=logMinusExp(jitter,conditionalEmisLogProb(i,_)+enumLogProb)-absorbLogProb;
   }
   return(asLogProb);
 }
